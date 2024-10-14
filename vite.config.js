1c7fc940d51352e7b99d925c669e0df627091d52
Coder Adarsh <adarshsinha961@gmail.com>
2024-10-14 14:26:21 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Todo-App-with-React/' // This is your GitHub repository name
+});
